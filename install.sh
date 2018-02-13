3eedca0fcf2e25ce6c2f4b51c29590df0bbadb22
Tareq Anwar <trqnwr@gmail.com>
2018-02-13 14:09:06 -0600

    remove -y  from mongodb installation

--- install.sh
+++ install.sh
@@ -55,7 +55,7 @@ echo "✔ installed nodejs, npm, nodemon, python"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 sudo apt-get update
-sudo apt-get install -y mongodb-org
+sudo apt-get install mongodb-org
 echo "✔ installed mongodb"
 
 sudo add-apt-repository ppa:ondrej/php
